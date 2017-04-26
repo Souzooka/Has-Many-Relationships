6376041fff79ab06f890a45b099910b430915a50
Dakota <bazookaharwell@gmail.com>
2017-04-26 11:55:11 -1000

    Query 13

--- joins.sql
+++ joins.sql
@@ -102,6 +102,12 @@ WHERE comments.body LIKE '%SSL%' AND posts.content LIKE '%dolorum%';
 --  username of the author of the comment (aliased to comment_author_username), and comment body (aliased to comment_body),
 --  where the comment body contains the word 'SSL' or 'firewall'
 --  and the post content contains the word 'nemo' ( should have 218 results )
+SELECT users.first_name AS post_author_first_name, users.last_name AS post_author_last_name,
+       users.username AS comment_author_username, comments.body AS comment_body
+FROM comments
+INNER JOIN posts ON comments.posts_id = posts.id
+INNER JOIN users ON comments.users_id = users.id
+WHERE (comments.body LIKE '%SSL%' OR comments.body LIKE '%firewall%') AND posts.content LIKE '%nemo%';
 
 
 --BONUS DUCKS
